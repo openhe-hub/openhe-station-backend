cd335ad24fe34642415a1ffe49896091582e8167
openhe <625529334@qq.com>
2023-01-29 11:00:05 +0800

    1-29-init

---
+++ src/main/java/com/openhe/backend/OpenheStationBackendApplication.java
@@ -0,0 +1,11 @@
+package com.openhe.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenheStationBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenheStationBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/com/openhe/backend/OpenheStationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openhe.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenheStationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
